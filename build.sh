ba0a759bdce2773c85fe18f0c196e5b3e4721542
James Pirruccello <james@carbocation.com>
2021-07-19 00:17:58 -0400

    Install the CUDA drivers to try to avoid issues raised in https://github.com/deepmind/alphafold/issues/11 and https://github.com/deepmind/alphafold/issues/26

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # docker build -f docker/Dockerfile -t alphafold .
-docker build -f docker/Dockerfile --tag="gcr.io/broad-ml4cvd/afdb:v20210718" --network=host .
+# docker build -f docker/Dockerfile --tag="gcr.io/broad-ml4cvd/afdb:v20210718" --network=host .
+docker build -f docker/Dockerfile --tag="gcr.io/broad-ml4cvd/afdb:v20210719" --network=host .
 
 # Note that:
 # DOWNLOAD_DIR = os.environ['ALPHADB_DIR']
